59f54e5dba10e42bfff8793c2d6f5017447ba1ee
Ilya Makarov <i.o.makarov@2gis.ru>
2024-03-13 16:03:35 +0300

    Added utf-8 check plugin implementation

--- src/dgis/hooks/plugins/packaged/utf8_check.py
+++ src/dgis/hooks/plugins/packaged/utf8_check.py
@@ -1,11 +1,16 @@
+import codecs
+
 from pathlib import Path
 
 from dgis.hooks.plugins.plugin import Plugin, PluginContext, PluginResult, PluginResultStatus
 
 
 class UTF8CheckPlugin(Plugin):
+    _file_extensions_to_check = [".cpp", ".h", ".c", ".hpp", ".hqt", ".json", ".xml", ".txt", ".md"]
+
     @classmethod
     def execute(cls, context: PluginContext) -> PluginResult:
+        errors = {}
         diff = context.ref.diff(context.repo)
         for diff_content in diff:
             if diff_content.deleted_file:
@@ -15,16 +20,19 @@ class UTF8CheckPlugin(Plugin):
             if not file_path.exists():
                 continue
 
-            if file_path.suffix.lower() not in [".cpp", ".h", ".c", ".hpp", ".hqt", ".json", ".xml", ".txt", ".md"]:
+            if file_path.suffix.lower() not in cls._file_extensions_to_check:
                 continue
 
-            with open(file_path, mode="rb") as file:
-                # TODO check UTF-8
-                pass
+            try:
+                codecs.open(file_path, encoding='utf-8', errors='strict').readlines()
+            except UnicodeDecodeError as error:
+                errors["file_path"] = error
 
-        return PluginResult(PluginResultStatus.Ok, None)
+        return PluginResult(PluginResultStatus.Failed, errors) if errors else PluginResult(PluginResultStatus.Ok, None)
 
     @classmethod
     def post_execute(cls, context: PluginContext, result: PluginResult):
-        assert result.status == PluginResultStatus.Ok
-        pass
+        if not context.log or result.status == PluginResultStatus.Ok:
+            return
+        for file_path, error in result.data.items():
+            context.log.error(f"Check JSON failed for file: '{file_path}' with error: '{error}'")
