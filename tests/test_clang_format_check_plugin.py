6531522bb27dccae7af7958e089b96727daa1477
Ilya Makarov <i.o.makarov@2gis.ru>
2024-05-17 11:22:28 +0300

    Added test to rename file without diff

--- tests/test_clang_format_check_plugin.py
+++ tests/test_clang_format_check_plugin.py
@@ -7,7 +7,7 @@ from dgis.hooks.utility.git import GitRef
 from dgis.hooks.plugins.plugin import PluginContext, PluginResultStatus, execute_plugin
 from dgis.hooks.plugins.packaged.clang_format_check import ClangFormatCheckPlugin
 
-from utility import make_and_commit_test_file
+from utility import make_and_commit_test_file, move_and_commit_test_file
 
 g_clang_format_content = """
 ---
@@ -144,3 +144,20 @@ def test_cpp_non_empty_files_invalid_format(tmp_path, cpp_content):
 
     with execute_plugin(ClangFormatCheckPlugin, context) as result:
         assert result.status == PluginResultStatus.Failed
+
+
+@pytest.mark.parametrize("cpp_content", _g_cpp_files)
+def test_cpp_non_empty_files_moved(tmp_path, cpp_content):
+    git_repo_path = tmp_path / "tmp-rep"
+    git_repo = Repo.init(git_repo_path)
+
+    make_and_commit_test_file(git_repo, Path(".clang-format"), g_clang_format_content)
+    make_and_commit_test_file(git_repo, Path("test.cpp"), cpp_content)
+
+    move_and_commit_test_file(git_repo, Path("test.cpp"), Path("test_2.cpp"))
+
+    ref = GitRef(git_repo.commit("HEAD~1").hexsha, git_repo.commit("HEAD").hexsha, git_repo.head.ref.name)
+    context = PluginContext(ref, git_repo, None)
+
+    with execute_plugin(ClangFormatCheckPlugin, context) as result:
+        assert result.status == PluginResultStatus.Ok
