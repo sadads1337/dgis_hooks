c94a99fa50bf7cfb823e1513dab4779aac50fb9c
Ilya Makarov <i.o.makarov@2gis.ru>
2026-03-23 13:13:06 +0300

    Small typing fixes

--- src/dgis/hooks/utility/log.py
+++ src/dgis/hooks/utility/log.py
@@ -1,12 +1,13 @@
 import logging
+from typing import Optional
 
-_log = None
+_log: Optional[logging.Logger] = None
 
 
-def init_log(name: str):
+def init_log(name: str) -> logging.Logger:
     global _log
     if _log:
-        return
+        return _log
     _log = logging.getLogger(name)
     _log.setLevel(logging.INFO)
     ch = logging.StreamHandler()
@@ -17,17 +18,22 @@ def init_log(name: str):
     return _log
 
 
-def log_debug(message: str):
-    _log.debug(message)
+def _get_logger() -> logging.Logger:
+    # Return the configured logger if present, otherwise a default logger
+    return _log if _log is not None else logging.getLogger(__name__)
 
 
-def log_info(message: str):
-    _log.info(message)
+def log_debug(message: str) -> None:
+    _get_logger().debug(message)
 
 
-def log_warning(message: str):
-    _log.warning(message)
+def log_info(message: str) -> None:
+    _get_logger().info(message)
 
 
-def log_error(message: str):
-    _log.error(message)
+def log_warning(message: str) -> None:
+    _get_logger().warning(message)
+
+
+def log_error(message: str) -> None:
+    _get_logger().error(message)
